b10c9ad4ce3f76e9979277edcf93b061b3d0ef2d
osnat ben ari <osnat122ster@gmail.com>
2020-08-21 20:02:09 +0300

    in commit

---
+++ 1.1_yes_or_no.js
@@ -0,0 +1,14 @@
+/*
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false
+*/
+
+boolean = false;
+
+function booleanToString(booleanValue) {
+    if(booleanValue) {
+        return 'Yes'
+    }
+    return 'No'
+}
+
+console.log(booleanToString(boolean));
