86d45ae3b70d5281d013c0e59c30139de37bb6fe
osnat ben ari <osnat122ster@gmail.com>
2020-08-22 13:16:09 +0300

    2.6

---
+++ 2.6_years_and_Centuries.js
@@ -0,0 +1,35 @@
+/*
+
+The first century spans from the year 1 up to and including the year 100, The second - from the year 101 up to and including the year 200, etc.
+Task :
+Given a year, return the century it is in.
+Input , Output Examples ::
+centuryFromYear(1705)  returns (18)
+centuryFromYear(1900)  returns (19)
+centuryFromYear(1601)  returns (17)
+centuryFromYear(2000)  returns (20)
+
+*/
+// this function is rigth only for ater year 0!!
+function centuryFromYear(year) {
+    let century;
+    if(year % 100 === 0 ) {
+        century = year / 100 ;
+    } else {
+        century = parseInt(year / 100) + 1;
+    }
+    console.log(century);
+}
+
+centuryFromYear(-101)
+
+
+// another option- more accurate function to find century !:
+
+//The Math.ceil() function always rounds a number up to the next largest integer.
+
+function century(year) {
+    return Math.ceil(year / 100);
+}
+
+console.log(century(-101));
